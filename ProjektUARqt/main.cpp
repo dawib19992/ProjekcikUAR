16615f4a2a666c99b578ab917db496fc0643389b
Rychu Partyka <partykaryszard@outlook.com>
2025-01-14 18:17:27 +0100

    Wygląd Aplikacji #01 - wgranie ikony

--- ProjektUARqt/main.cpp
+++ ProjektUARqt/main.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("./programicon.png"));
     UkladAutomatycznejRegulacji w;
+    w.setWindowTitle("Układ Automatycznej Regulacji");
+    w.setWindowIcon(QIcon("./programicon.png"));
     w.show();
     return a.exec();
 }
